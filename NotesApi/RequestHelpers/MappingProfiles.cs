36967661edbc1c223d83406b5c43db6d6a3a404c
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-05 17:18:34 +0100

    added a mapping profile for the create category DTO.

---
+++ NotesApi/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using NotesApi.DTOs.Categories;
+using NotesApi.Models;
+
+namespace NotesApi.RequestHelpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CreateCategoryDTO, Category>();
+        }
+    }
+}
