7dcd9ed38c2b1e96233c66fec7662fe75f57585b
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-05 16:20:50 +0100

    added note model.

---
+++ NotesApi/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace NotesApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public int CategoryId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
