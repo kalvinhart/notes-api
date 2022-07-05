460c171818d3ba528fe0f08efd1cdfe772e7da10
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-05 16:33:52 +0100

    added database.

---
+++ NotesApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApi.Models;
+
+namespace NotesApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Note> Notes { get; set; } = null!;
+    }
+}
