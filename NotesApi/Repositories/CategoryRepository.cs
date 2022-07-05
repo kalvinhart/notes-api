1f88f8bc96e171e923794fd826940e0e56d13d23
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-05 17:48:16 +0100

    added categories repository.

---
+++ NotesApi/Repositories/CategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesApi.Data;
+using NotesApi.Models;
+
+namespace NotesApi.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext _context;
+
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Category> CreateCategory(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Boolean> DeleteCategory(int id)
+        {
+            var categoryToDelete = await _context.Categories.FindAsync(id);
+            if (categoryToDelete == null) return false;
+
+            _context.Categories.Remove(categoryToDelete);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public async Task<List<Category>> GetAllCategories()
+        {
+            return  await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategory(int id)
+        {
+            return await _context.Categories.Include(c => c.Notes).FirstAsync(c => c.Id == id);
+        }
+    }
+}
---
+++ NotesApi/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using NotesApi.Models;
+
+namespace NotesApi.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllCategories();
+        Task<Category> GetCategory(int id);
+        Task<Category> CreateCategory(Category category);
+        Task<Boolean> DeleteCategory(int id);
+    }
+}
